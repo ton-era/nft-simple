078b3f446d4c9602827a033f20fb275dd7120819
sergei.polianskikh <sergei.polianskikh@plarium.com>
2022-04-11 22:57:58 +0300

    NFT mint SUCCESS!

--- src/api.sh
+++ src/api.sh
@@ -21,9 +21,11 @@ then
     seqno=$2
     coll_init_ng=$3
     $FIFT_EXE_PATH -s $REQUESTS_PATH/nft-collection-deploy.fif $seqno $coll_init_ng
-elif [[ $command == "get_collection_data" ]]
+elif [[ $command == "mint" ]]
 then
-    echo "no yet!"
+    seqno=$2
+    item_init_ng=$3
+    $FIFT_EXE_PATH -s $REQUESTS_PATH/nft-item-mint.fif $seqno $item_init_ng
 else
     echo "Unknown command"
 fi
