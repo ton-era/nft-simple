1273acf4f5b2bd9c4418eb9bd6ce4ef40e425e84
sergei.polianskikh <sergei.polianskikh@plarium.com>
2022-04-13 22:38:28 +0300

    ref

--- src/api.sh
+++ src/api.sh
@@ -16,19 +16,28 @@ echo
 echo "Running command: " $command " ..."
 echo
 
+
 if [[ $command == "deploy" ]]
 then
     seqno=$2
-    coll_init_ng=$3
-    $FIFT_EXE_PATH -s $REQUESTS_PATH/nft-collection-deploy.fif $seqno $coll_init_ng
+    collection_content_uri=$3
+    item_content_base_uri=$4
+    royalty_base=$5
+    royalty_factor=$6
+    coll_init_ng=$7
+    secret_path=$8
+    addr_path=$9
+    $FIFT_EXE_PATH -s $REQUESTS_PATH/nft-collection-deploy.fif $seqno $collection_content_uri $item_content_base_uri $royalty_base $royalty_factor $coll_init_ng $secret_path $addr_path
 elif [[ $command == "mint" ]]
 then
     seqno=$2
-    item_index=$3
-    coll_address=$4
+    coll_address=$3
+    item_index=$4
     coll_ng=$5
     item_ng=$6
-    $FIFT_EXE_PATH -s $REQUESTS_PATH/nft-item-mint.fif $seqno $item_index $coll_address $coll_ng $item_ng
+    secret_path=$7
+    addr_path=$8
+    $FIFT_EXE_PATH -s $REQUESTS_PATH/nft-item-mint.fif $seqno $coll_address $item_index $coll_ng $item_ng $secret_path $addr_path
 else
     echo "Unknown command"
 fi
